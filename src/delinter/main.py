cdca054b12b8b4f720717da71f217cb9f0a7aa3f
grdvnl <grdvnl@gmail.com>
2019-10-22 06:36:59 -0700

    handle absolute and relative paths

--- src/delinter/main.py
+++ src/delinter/main.py
@@ -15,30 +15,27 @@ also be used as template for Python modules.
 Note: This skeleton file can be safely removed if not needed!
 """
 
-import argparse
-import sys
-import logging
-
 import os
 import re
+import sys
 import typing as tp
 import difflib
+import logging
 import argparse
 import collections
 import dataclasses
 from typing import Set
 from typing import Dict
 from typing import Union
+from pathlib import Path
 from collections import defaultdict
 
+import libcst as cst
 from pylint import epylint as lint
 
-import libcst as cst
+from delinter import __version__
 from delinter.unused_imports import UnusedImportsDelinter
 from delinter.unused_imports import RemoveUnusedImportTransformer
-
-
-from delinter import __version__
 
 __author__ = "grdvnl"
 __copyright__ = "grdvnl"
@@ -64,7 +61,6 @@ class Delinter:
 
         if msg_id not in SUPPORTED_LINTER_MAP:
             raise ValueError(f'{msg_id} not currently supported for delinting.')
-
         parsed_warnings = []
         for warning in warnings:
             m = re.match(cls.pattern, warning)
@@ -162,23 +158,27 @@ def _run_delinter(options):
     Run the delinter and produce the diff.
     '''
     root_file_path = options.file_path_or_folder
-    msg_template = r'{path}:{line}:[{msg_id}({symbol}),{obj}]{msg}'
+    if Path(root_file_path).is_absolute():
+        msg_template = r'{abspath}:{line}:[{msg_id}({symbol}),{obj}]{msg}'
+    else:
+        msg_template = r'{path}:{line}:[{msg_id}({symbol}),{obj}]{msg}'
     pylint_command = f"{root_file_path} --enable=W --disable=C,R,E,F --msg-template={msg_template} --score=n"
 
     out, _ = lint.py_run(pylint_command, return_std=True)
-    result = "".join(out.readlines()).split('\n')
-    result = [r.strip() for r in result if r.strip() and not r.strip().
+    orig_result = "".join(out.readlines()).split('\n')
+    result = [r.strip() for r in orig_result if r.strip() and not r.strip().
             startswith('************* Module ')]
     parsed_warnings = Delinter.parse_linter_warnings(result, options.msg_id)
     if os.path.isdir(root_file_path):
-        from pathlib import Path
-        files = Path(root_file_path).glob('**/*.py')
+        files = list(Path(root_file_path).glob('**/*.py'))
     else:
         files = [root_file_path]
 
     for file_path in files:
         with open(file_path) as f:
             source_code = "".join(f.readlines())
+            if not source_code:
+                continue
             source_tree = cst.parse_module(source_code)
             wrapper = cst.MetadataWrapper(source_tree)
             local_warnings = [p for p in parsed_warnings if p.file_path == str(file_path)]
@@ -186,12 +186,14 @@ def _run_delinter(options):
                     SUPPORTED_LINTER_MAP[options.msg_id][1](local_warnings))
             a_file_path = 'a/' + str(file_path)
             b_file_path = 'b/' + str(file_path)
-            print("".join(difflib.unified_diff(
+            result = "".join(difflib.unified_diff(
                     source_code.splitlines(1),
                     fixed_module.code.splitlines(1),
                     fromfile=a_file_path,
                     tofile=b_file_path
-                    )))
+                    ))
+            if result:
+                print(result)
 
 
 
@@ -213,7 +215,7 @@ def main(args=None):
       args ([str]): command line parameter list
     """
     args = get_arg_parser().parse_args(args)
-    setup_logging(args.loglevel)
+    #setup_logging(args.loglevel)
     _logger.debug('Starting the pydelint process...')
     _run_delinter(args)
     _logger.debug('pydeling complete')
